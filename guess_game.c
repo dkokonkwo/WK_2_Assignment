0da3095b7b9ac34102405a1dac102073fb20fe74
dkokonkwo <d.okonkwo@alustudent.com>
2024-08-01 15:57:11 -0300

    fixed float bug

--- guess_game.c
+++ guess_game.c
@@ -6,45 +6,44 @@
 int main(void)
 {
 srand(time(0));
-int rand_number = rand() % 100 + 1;
+int rand_number = rand() % 101;
 int answer, tries = 10;
 int correct = 0;
 
 printf("Welcome to the ultimate challenge for testing your luck!!\n");
 printf("Guess a number between 0 and 100 that I've chosen. You have %d tries\n", tries);
-printf("First try: ");
-scanf("%d", &answer);
 
 while (!correct && tries > 0)
 {
-if (isdigit(answer))
+printf("Your guess: ");
+if (scanf("%d", &answer) == 1)
 {
 if (answer < rand_number)
 {
 tries--;
-printf("Oops! You guess is smaller, try again (%d tries left): ", tries);
-scanf("%d", &answer);
+printf("Oops! You guess is smaller, try again (%d tries left). ", tries);
 }
 else if (answer > rand_number)
 {
 tries--;
-printf("Nope! too big, try again (%d tries left): ", tries);
-scanf("%d", &answer);
+printf("Nope! too big, try again (%d tries left). ", tries);
 }
 else
 {
 printf("Legend! That's correct. What were the chances huh?\n");
-return (0);
 correct = 1;
 }
 }
 else
 {
-printf("Enter a whole number between 0 and 100\n");
-return(-1);
+while (getchar() != '\n');
+printf("Invalid input. Enter a whole number between 0 and 100\n");
 }
 }
 
-printf("Sorry out of tries. Try again\n");
-return(-1);
+if (!correct)
+{
+printf("\nSorry out of tries. Try again\n");
+return(0);
+}
 }
